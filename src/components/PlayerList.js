5ba26526c3b3ece4ddd2fa44df4e86bfcf0a0426
brysonbest <brycekalden@gmail.com>
2022-02-16 17:29:55 -0800

    Add helpers to parse player and match data.

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Player from "./Player";
+import playerData from "../data/playerData";
+import matchData from "../data/matchData";
+import { addWinsToPlayers, preparePlayerData } from "../helpers/playerHelpers";
 
-function PlayerList() {
+function PlayerList(props) {
+  
+  const playerDataArray = preparePlayerData(playerData);
+  // console.log('player!', playerDataArray);
+  // eslint-disable-next-line
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+
+  const onePlayer = parsedPlayerData[0];
+  // console.log(parsedPlayerData);
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player />
+      <Player firstName={onePlayer.firstName} lastName={onePlayer.lastName} gamerTag={onePlayer.gamerTag} wins={onePlayer.wins} />
     </section>
   );
 }
