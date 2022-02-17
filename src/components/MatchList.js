b2d751530fd70acb0295cc4159c42789be7a00ea
brysonbest <brycekalden@gmail.com>
2022-02-16 17:49:41 -0800

    Refactor to handle array mapping for players and matches.

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Match from "./Match";
-import matchData from "../data/matchData";
 
 function MatchList(props) {
 
-  const oneMatch = matchData[0];
+  const matches = props.matchData.map((match) => {
+    return(
+    <Match players={match.players} winner={match.winner} scoreDifference={match.scoreDifference} key={match.matchNumber} />
+    );
+  });
+
+  const oneMatch = props.matchData[0];
 
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
-      <Match players={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
+      {matches}
     </section>
   );
 }
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,22 +1,19 @@
 import React from "react";
 import Player from "./Player";
-import playerData from "../data/playerData";
-import matchData from "../data/matchData";
 import { addWinsToPlayers, preparePlayerData } from "../helpers/playerHelpers";
 
 function PlayerList(props) {
   
-  const playerDataArray = preparePlayerData(playerData);
-  // console.log('player!', playerDataArray);
-  // eslint-disable-next-line
-  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const players = props.parsedPlayerData.map((player) => {
+    return(
+      <Player firstName={player.firstName} lastName={player.lastName} gamerTag={player.gamerTag} wins={player.wins} key={player.gamerTag} />
+    )
+  })
 
-  const onePlayer = parsedPlayerData[0];
-  // console.log(parsedPlayerData);
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player firstName={onePlayer.firstName} lastName={onePlayer.lastName} gamerTag={onePlayer.gamerTag} wins={onePlayer.wins} />
+      {players}
     </section>
   );
 }
